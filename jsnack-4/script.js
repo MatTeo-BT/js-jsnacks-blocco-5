7b6a152b75b72ec5c0f827a94cfb4da00849efe9
MatTeo-BT <aboutmet@gmail.com>
2023-11-28 12:53:54 +0100

    snack da rivedere

---
+++ jsnack-4/script.js
@@ -0,0 +1,17 @@
+// Scrivi una funzione che fonda due array 
+// (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall'altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const firstArray = [`a`, `b`, `c`, `d`, `e`];
+const secondArray = [1, 2, 3, 4, 5];
+
+
+if (secondArray.lenght !== firstArray){
+    alert (`impossible merge`)
+} else {
+    const newArray = [];
+    secondArray.forEach ((element , index)=> {
+
+    })
+}
+
